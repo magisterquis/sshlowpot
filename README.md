9c1a95e59947f8469c97cc85094c1d0ae894cd01
Stuart <kd5pbo@gmail.com>
2016-01-19 19:55:48 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # sshlowpot
 Yet another no-frills low-interaction ssh honeypot in Go.
+
+Accepts SSH connections on a given port (2222 by default), records
+authentication attempts and tells the connecting client the authentication
+failed.
+
+## Installation
+Get and build the source:
+```bash
+go get github.com/kd5pbo/sshlowpot
+go install github.com/kd5pbo/sshlowpot
+```
+Compiled binaries can be made available upon request.
+
+Make sure IP forwarding is enabled and forward the port.  The following
+examples assume the external-facing port is 22 and sshlowpot is listening on
+2222.
+
+OpenBSD:
+```bash
+Assuming the external-facing interface is vio0
+
+# sysctl net.inet.ip.forwarding=1
+# echo "pass in on vio0 from any to (vio0) port 22 rdr-to 127.0.0.1 port 2222 >> /etc/pf.conf
+# pfctl -vf /etc/pf.conf
+$ sshlowopt -v
+```
+
+Linux:
+```bash
+# sysctl net.ipv4.ip_forward=1
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j REDIRECT --to-port 2222
+$ sshlowpot -v
+```
+ 
+## Usage
+```bash
+Usage: sshlowpot [options]
+
+Options are:
+  -a string
+    	Listen address (default "127.0.0.1:2222")
+  -key string
+    	SSH private key file, which will be created if it doesn't already exist (default "id_rsa")
+  -v	Enable verbose logging
+  -ver string
+    	SSH server version string (default "SSH-2.0-OpenSSH_7.0")
+```
+
+For the most part, no options are required as long as you can forward your
+external port of choice to 127.0.0.1:2222.  Please don't run it as root on 22.
+
+## Output
+Output should look something like the following (with -v):
+```
+2016/01/19 19:43:41 Made SSH key and wrote it to id_rsa
+2016/01/19 19:43:41 Listening on 127.0.0.1:2222
+2016/01/19 19:43:51 Address:168.235.89.22:52119 Connect
+2016/01/19 19:43:53 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Key(ssh-rsa):BE9DA2A4D129652DB64AF6D71DEFD25F
+2016/01/19 19:43:56 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Keyboard-Interactive:"passtry1"
+2016/01/19 19:43:57 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Keyboard-Interactive:"passtry2"
+2016/01/19 19:43:58 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Keyboard-Interactive:"passtry3"
+2016/01/19 19:43:59 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Password:"passtry4"
+2016/01/19 19:44:01 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Password:"passtry5"
+2016/01/19 19:44:02 Address:168.235.89.22:52119 User:"exuser" Version:"SSH-2.0-OpenSSH_7.0" Password:"passtry6"
+2016/01/19 19:44:02 Address:168.235.89.22:52119 Disconnect
+```
+
+## Windows
+It should run on Windows just fine.  If it doesn't, feel free to send a pull
+request.
---
+++ sshlowpot.go
@@ -0,0 +1,283 @@
+//sshlowpot is a low-interaction ssh honeypot
+package main
+
+/*
+ * sshlowpot.go
+ * Low-interaction honeypot
+ * By J. Stuart McMurray
+ * Created 20160119
+ * Last Modified 20160119
+ */
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+var verbon = flag.Bool(
+	"v",
+	false,
+	"Enable verbose logging",
+)
+
+func main() {
+	var (
+		addr = flag.String(
+			"a",
+			"127.0.0.1:2222",
+			"Listen address",
+		)
+		sver = flag.String(
+			"ver",
+			"SSH-2.0-OpenSSH_7.0",
+			"SSH server version string",
+		)
+		privKey = flag.String(
+			"key",
+			"id_rsa",
+			"SSH private key file, which will be created if it "+
+				"doesn't already exist",
+		)
+	)
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage: %v [options]
+
+Options are:
+`,
+
+			os.Args[0],
+		)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	/* Server Config */
+	conf, err := serverConfig(*sver, *privKey)
+	if nil != err {
+		log.Fatalf("Unable to generate server config: %v", err)
+	}
+
+	/* Listen on the address */
+	l, err := net.Listen("tcp", *addr)
+	if nil != err {
+		log.Fatalf("Unable to listen on %v: %v", *addr, err)
+	}
+	log.Printf("Listening on %v", l.Addr())
+
+	/* Pop off connections, handle them */
+	for {
+		c, err := l.Accept()
+		if nil != err {
+			log.Fatalf("Unable to accept new connection: %v", err)
+		}
+		go handle(c, conf)
+	}
+}
+
+/* Verbose logging */
+func verbose(f string, a ...interface{}) {
+	if !*verbon {
+		return
+	}
+	log.Printf(f, a...)
+}
+
+/* serverConfig makes an SSH server config struct with server version string
+sv and private key from file named pkf. */
+func serverConfig(sv, pkf string) (*ssh.ServerConfig, error) {
+	/* Config to return */
+	c := &ssh.ServerConfig{
+		/* Log authentication */
+		PasswordCallback:            logPass,
+		PublicKeyCallback:           logPubKey,
+		KeyboardInteractiveCallback: logKeyInt,
+
+		/* Server Version string */
+		ServerVersion: sv,
+	}
+	/* Try to open the private key file */
+	privateKeyFile, err := os.OpenFile(pkf, os.O_RDWR|os.O_CREATE, 0600)
+	if nil != err {
+		return nil, err
+	}
+	defer privateKeyFile.Close()
+
+	/* Read the file's contents */
+	pkb, err := ioutil.ReadAll(privateKeyFile)
+	if nil != err {
+		return nil, err
+	}
+	/* If the file was empty, make a key, write the file */
+	if 0 == len(pkb) {
+		pkb, err = makeKeyInFile(privateKeyFile)
+		if nil != err {
+			return nil, err
+		}
+	} else {
+		verbose("Read SSH key file %v", pkf)
+	}
+	/* Parse the key */
+	pk, err := ssh.ParsePrivateKey(pkb)
+	if nil != err {
+		return nil, err
+	}
+	/* Add it to the config */
+	c.AddHostKey(pk)
+	/* Return the config */
+	return c, nil
+}
+
+/* makeKeyInFile makes a private SSH key and writes it to the file f, and
+ * returns what it wrote to the file. */
+func makeKeyInFile(f *os.File) ([]byte, error) {
+	/* Below code mooched from
+	 * http://stackoverflow.com/questions/21151714/go-generate-an-ssh-public-key */
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+	privateKeyPEM := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	/* Encode the key */
+	pkb := pem.EncodeToMemory(privateKeyPEM)
+
+	/* Try to write it to the file */
+	if _, err := f.Write(pkb); nil != err {
+		return nil, err
+	}
+	verbose("Made SSH key and wrote it to %v", f.Name())
+
+	/* Return the bytes */
+	return pkb, nil
+}
+
+/* logPass logs a password attempt (and returns failure) */
+func logPass(
+	conn ssh.ConnMetadata,
+	password []byte,
+) (*ssh.Permissions, error) {
+	log.Printf("%v Password:%q", ci(conn), password)
+	return nil, fmt.Errorf("invalid password")
+}
+
+/* logPubKey logs a public key attempt */
+func logPubKey(
+	conn ssh.ConnMetadata,
+	key ssh.PublicKey,
+) (*ssh.Permissions, error) {
+	log.Printf(
+		"%v Key(%v):%02X",
+		ci(conn),
+		key.Type(),
+		md5.Sum(key.Marshal()),
+	)
+	return nil, fmt.Errorf("invalid key")
+}
+
+/* logKeyInt logs a keyboard-interactive attempt */
+func logKeyInt(
+	conn ssh.ConnMetadata,
+	client ssh.KeyboardInteractiveChallenge,
+) (*ssh.Permissions, error) {
+	/* Send a welcome and the prompt */
+	a, err := client(
+		conn.User(),
+		"",
+		[]string{"Password: "},
+		[]bool{false},
+	)
+	/* Handle responses */
+	if nil != err { /* Something went wrong */
+		log.Printf(
+			"%v error sending keyboard-interactive prompt: %v",
+			ci(conn),
+			err,
+		)
+	} else if 0 == len(a) { /* Didn't get an answer */
+		log.Printf("%v no keyboard-interactive answer")
+	} else if 1 < len(a) { /* We mysteriously got back too many answers */
+		for i, v := range a {
+			log.Printf(
+				"%v Keyboard-Interactive-%v/%v: %q",
+				ci(conn),
+				i,
+				len(a),
+				v,
+			)
+		}
+	} else { /* Got just one answer */
+		log.Printf(
+			"%v Keyboard-Interactive:%q",
+			ci(conn),
+			a[0],
+		)
+	}
+	return nil, fmt.Errorf("invalid password")
+}
+
+/* ci returns a string containing info from an ssh.ConnMetadata */
+func ci(m ssh.ConnMetadata) string {
+	return fmt.Sprintf(
+		"Address:%v User:%q Version:%q",
+		m.RemoteAddr(),
+		m.User(),
+		m.ClientVersion(),
+	)
+}
+
+/* handle handles a new connection */
+func handle(c net.Conn, conf *ssh.ServerConfig) {
+	verbose("Address:%v Connect", c.RemoteAddr())
+	/* Upgrade to an SSH connection */
+	sc, chans, reqs, err := ssh.NewServerConn(c, conf)
+	if nil != err {
+		/* EOF means all auth failed */
+		if io.EOF == err {
+			verbose("Address:%v Disconnect", c.RemoteAddr())
+			return
+		}
+		log.Printf(
+			"Unable to handle connection from %v: %v",
+			c.RemoteAddr(),
+			err,
+		)
+		c.Close()
+		return
+	}
+	defer sc.Close()
+	/* Ignore requests */
+	go ssh.DiscardRequests(reqs)
+	/* Reject channel requests */
+	for nc := range chans {
+		if err := nc.Reject(
+			ssh.Prohibited,
+			"not allowed",
+		); nil != err {
+			log.Printf(
+				"Error rejecting channel request for %v "+
+					"from %v: %v",
+				nc.ChannelType(),
+				c.RemoteAddr(),
+				err,
+			)
+		}
+	}
+
+}
